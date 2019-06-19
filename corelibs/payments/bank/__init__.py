6be2c2577104643af7ba6a8c75b534fe0c8c40ce
hainguyencse <hai.nguyen@alphabase.vn>
2019-06-19 11:10:16 +0700

    Update BankProvider

--- corelibs/payments/bank/__init__.py
+++ corelibs/payments/bank/__init__.py
@@ -30,10 +30,15 @@ class BankProvider(BasicProvider):
         coefficient = self.get_coefficient(currency_code=payment.currency)
 
         success_url = payment.get_success_url()
+        payment.change_status(PaymentStatus.PREAUTH)
         payment.attrs.bank = json.dumps(request.data['bank'])
         return success_url
 
     def capture(self, payment, amount=None):
+        # Check supported currencies
+        coefficient = self.get_coefficient(currency_code=payment.currency)
+
+        amount = int(amount or payment.total) * int(coefficient)
         payment.change_status(PaymentStatus.CONFIRMED)
         return amount
 
