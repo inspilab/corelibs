b5ea6405b6eb68c0e8bbffda15553c129cf2740c
hainguyencse <hai.nguyen@alphabase.vn>
2019-06-20 17:18:49 +0700

    Update payment model

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = open(os.path.join(here, 'README.md')).read()
 
 setup(
     name='corelibs',
-    version='1.4.7',
+    version='1.4.8',
     packages=[
         'corelibs',
         'corelibs.pubsub',
