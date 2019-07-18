02cb942089ea7fe8e7a4cc47744835e39fb14a6b
hainguyencse <hai.nguyen@alphabase.vn>
2019-07-18 12:10:53 +0700

    Update mixins

---
+++ corelibs/inspitrip/mixins.py
@@ -0,0 +1,13 @@
+class EVENT_TYPE:
+
+    ABANDONED_CART = 'abandoned-cart'
+    BOOKING_REQUEST = 'booking-request'
+    BOOKING_CONFIRM = 'booking-confirm'
+    BOOKING_CANCEL = 'booking-cancel'
+    BOOKING_FEEDBACK = 'booking-feedback'
+    BOOKING_REMIND_FEEDBACK = 'booking-remind-feedback'
+    LOCAL_BOOKING_CONFIRM = 'local-booking-confirm'
+    USER_RESET_PASSWORD = 'user-reset-password'
+    USER_ACTIVE = 'user-activate'
+    USER_WELCOME = 'user-welcome'
+
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = open(os.path.join(here, 'README.md')).read()
 
 setup(
     name='corelibs',
-    version='1.4.24',
+    version='1.4.25',
     packages=[
         'corelibs',
         'corelibs.pubsub',
