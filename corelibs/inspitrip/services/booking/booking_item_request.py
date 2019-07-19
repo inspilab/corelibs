e93676ce4dd707f93618074250fc11c3eb846fc6
hainguyencse <hai.nguyen@alphabase.vn>
2019-07-19 18:07:48 +0700

    Add booking services

---
+++ corelibs/inspitrip/services/booking/booking_item_request.py
@@ -0,0 +1,30 @@
+import requests
+import json
+from requests.exceptions import HTTPError
+from .constants import BOOKING_API_URL
+
+
+class BookingItemRequest:
+    @classmethod
+    def booking_item_url(cls, id):
+        url = "%s/api/booking-items/%s" % (
+            BOOKING_API_URL, id
+        )
+        return url
+
+    @classmethod
+    def get_booking_item(cls, id):
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        }
+        url = cls.booking_item_url(id)
+        try:
+            res = requests.get(url, headers=headers)
+            res.raise_for_status()
+        except HTTPError as http_e:
+            # Status is NOT 2xx
+            raise http_e
+
+        response = res.json()
+        return response
