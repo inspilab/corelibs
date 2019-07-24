9f8759ec5d4907137c7a9171871daa92ddc68a32
hainguyencse <hai.nguyen@alphabase.vn>
2019-07-24 16:35:00 +0700

    Update booking service

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = open(os.path.join(here, 'README.md')).read()
 
 setup(
     name='corelibs',
-    version='1.4.27',
+    version='1.4.28',
     packages=[
         'corelibs',
         'corelibs.pubsub',
