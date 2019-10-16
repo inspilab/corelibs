9eee4d5573947eea0a9fc402775d2c37201aa86a
hainguyencse <hai.nguyen@alphabase.vn>
2019-10-16 17:47:30 +0700

    Update version 1.7.6

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = open(os.path.join(here, 'README.md')).read()
 
 setup(
     name='corelibs',
-    version='1.7.5',
+    version='1.7.6',
     packages=[
         'corelibs',
         'corelibs.pubsub',
